85e3a4b04cb8ce7c1ec09fa8b11a27d7c41e3604
Browning <oet5@cdc.gov>
2020-11-18 00:55:57 -0500

    Adding in hyperparameter tuning code

--- python/keras_models.py
+++ python/keras_models.py
@@ -7,6 +7,7 @@ import os
 import pickle as pkl
 import random
 
+import kerastuner.tuners as tuners
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -15,15 +16,15 @@ import tools.keras as tk
 
 # %% Globals
 sample_n = 10000
-n_grams_setting = None
 time_seq = 225
 lstm_dropout = 0.2
 lstm_recurrent_dropout = 0.2
 n_lstm = 128
-
+HYPER_TUNING = True
+BATCH_SIZE = 32
 # %% Load in Data
-output_dir = os.path.abspath("../output/") + "/"
-data_dir = os.path.abspath("../data/data/") + "/"
+output_dir = os.path.abspath("output/") + "/"
+data_dir = os.path.abspath("data/data/") + "/"
 pkl_dir = output_dir + "pkl/"
 
 with open(pkl_dir + "int_seqs.pkl", "rb") as f:
@@ -49,39 +50,62 @@ lens = [[len(x) for x in y] for y in X]
 n_bags = max(itertools.chain(*lens))
 
 # %% Create data generator for On-the-fly batch generation
-dat_generator = tk.DataGenerator(inputs=X, labels=y, dim=[time_seq, n_bags])
+dat_generator = tk.DataGenerator(inputs=X,
+                                 labels=y,
+                                 dim=[time_seq, n_bags],
+                                 batch_size=BATCH_SIZE)
 
 # %% Model
 
-input_layer = keras.Input(
-    shape=(time_seq, n_bags),
-    batch_size=32,
-)
-# NOTE: Not sure if we need to mask ragged or not, but if so, there should
-# be a masking layer here and the embedding layer should be set to ignore
-# 0 as a mask and input_dim=n_tok+1 accordingly
-emb_layer = keras.layers.Embedding(n_tok, output_dim=1,
-                                   input_length=time_seq)(input_layer)
+if HYPER_TUNING:
+    # Generate Hyperparameter model
+    hyper_model = tk.LSTMHyperModel(ragged=False,
+                                    n_timesteps=time_seq,
+                                    n_tokens=n_tok,
+                                    n_bags=n_bags,
+                                    batch_size=BATCH_SIZE)
+    tuner = tuners.Hyperband(
+        hyper_model,
+        objective="accuracy",
+        max_epochs=5,
+        project_name="hyperparameter-tuning",
+        distribution_strategy=tf.distribute.MirroredStrategy())
 
-# BUG: I think this is it? Maybe I need to look at the math again
-reshape = keras.layers.Reshape((time_seq, n_bags))(emb_layer)
+    # Announce the search space
+    tuner.search_space_summary()
 
-lstm_layer = keras.layers.LSTM(
-    n_lstm, dropout=lstm_dropout,
-    recurrent_dropout=lstm_recurrent_dropout)(reshape)
+    # And search the space
+    tuner.search(dat_generator, epochs=5)
 
-output_dim = keras.layers.Dense(1, activation="softmax")(lstm_layer)
+    # Get results
+    tuner.results_summary()
+else:
+    input_layer = keras.Input(
+        shape=(time_seq, n_bags),
+        batch_size=BATCH_SIZE,
+    )
+    # NOTE: Not sure if we need to mask ragged or not, but if so, there should
+    # be a masking layer here and the embedding layer should be set to ignore
+    # 0 as a mask and input_dim=n_tok+1 accordingly
+    emb_layer = keras.layers.Embedding(n_tok,
+                                       output_dim=1,
+                                       input_length=time_seq)(input_layer)
 
-model = keras.Model(input_layer, output_dim)
+    # BUG: I think this is it? Maybe I need to look at the math again
+    reshape = keras.layers.Reshape((time_seq, n_bags))(emb_layer)
 
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics="acc")
+    lstm_layer = keras.layers.LSTM(
+        n_lstm, dropout=lstm_dropout,
+        recurrent_dropout=lstm_recurrent_dropout)(reshape)
 
-model.summary()
+    output_dim = keras.layers.Dense(1, activation="softmax")(lstm_layer)
 
-# %% Train
-# NOTE: Multiprocessing is superfluous here with epochs=1, but we could use it
-model.fit_generator(generator=dat_generator,
-                    use_multiprocessing=True,
-                    workers=4)
+    model = keras.Model(input_layer, output_dim)
 
-# %%
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics="acc")
+
+    model.summary()
+
+    # %% Train
+    # NOTE: Multiprocessing is superfluous here with epochs=1, but we could use it
+    model.fit(dat_generator, use_multiprocessing=True, workers=4)
