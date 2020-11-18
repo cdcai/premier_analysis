465460ddc19ade91bc56f11b0467d45e14955f3e
beansrowning <sbrowning@cdc.gov>
2020-11-17 22:58:33 -0500

    Black failed me, so we in the YAPF gang now

--- python/keras_prep.py
+++ python/keras_prep.py
@@ -45,9 +45,8 @@ if NO_VITALS:
     ftr_cols = ["bill", "lab_res", "proc", "diag"]
 
 # Combining the separate feature columns into one
-trimmed_seq["ftrs"] = (
-    trimmed_seq[ftr_cols].astype(str).replace(["None", "nan"], "").agg(" ".join, axis=1)
-)
+trimmed_seq["ftrs"] = (trimmed_seq[ftr_cols].astype(str).replace(
+    ["None", "nan"], "").agg(" ".join, axis=1))
 
 # %% Fitting the vectorizer to the features
 ftrs = [doc for doc in trimmed_seq.ftrs]
@@ -64,11 +63,14 @@ with open(pkl_dir + "all_ftrs_dict.pkl", "wb") as f:
     pkl.dump(vocab, f)
 
 # Converting the bags of feature strings to integers
-int_ftrs = [[vocab[k] for k in doc.split() if k in vocab.keys()] for doc in ftrs]
+int_ftrs = [[vocab[k] for k in doc.split() if k in vocab.keys()]
+            for doc in ftrs]
 trimmed_seq["int_ftrs"] = int_ftrs
 
 # list of integer sequence arrays split by medrec_key
-int_seqs = [df.values for _, df in trimmed_seq.groupby("medrec_key")["int_ftrs"]]
+int_seqs = [
+    df.values for _, df in trimmed_seq.groupby("medrec_key")["int_ftrs"]
+]
 
 # Converting to a nested list to keep things clean
 seq_gen = [[seq for seq in medrec] for medrec in int_seqs]
@@ -80,10 +82,8 @@ if PAD_SEQS:
 # Starting to construct the labels part 1: figuring out which visit
 # were covid visits, and which patients have no covid visits (post-trim)
 cv_dict = dict(zip(pat_df.pat_key, pat_df.covid_visit))
-cv_pats = [
-    [cv_dict[pat_key] for pat_key in np.unique(seq.values)]
-    for _, seq in trimmed_seq.groupby("medrec_key").pat_key
-]
+cv_pats = [[cv_dict[pat_key] for pat_key in np.unique(seq.values)]
+           for _, seq in trimmed_seq.groupby("medrec_key").pat_key]
 no_covid = np.where([np.sum(doc) == 0 for doc in cv_pats])[0]
 
 # %% Removing the non-covid patients from seq_gen, cv_pats, and trimmed_seq
@@ -92,8 +92,9 @@ for n in no_covid:
     del seq_gen[n]
     medrec = trimmed_seq.iloc[n, 0]
     trimmed_seq.drop(
-        trimmed_seq.loc[trimmed_seq.medrec_key == medrec, :].index, axis=0, inplace=True
-    )
+        trimmed_seq.loc[trimmed_seq.medrec_key == medrec, :].index,
+        axis=0,
+        inplace=True)
 
 # %% Sanity check
 assert len(cv_pats) == len(seq_gen) == trimmed_seq.medrec_key.nunique()
@@ -101,20 +102,22 @@ assert len(cv_pats) == len(seq_gen) == trimmed_seq.medrec_key.nunique()
 # %% Part 2: figuring out how many feature bagsin each sequence belong
 # to each visit
 pat_lengths = trimmed_seq.groupby(["medrec_key", "pat_key"]).pat_key.count()
-pat_lengths = [[n for n in df.values] for _, df in pat_lengths.groupby("medrec_key")]
+pat_lengths = [[n for n in df.values]
+               for _, df in pat_lengths.groupby("medrec_key")]
 
 # %% Part 3: Figuring out whether a patient died after a visit
-died = np.array(
-    ["EXPIRED" in status for status in pat_df.disc_status_desc], dtype=np.uint8
-)
+died = np.array(["EXPIRED" in status for status in pat_df.disc_status_desc],
+                dtype=np.uint8)
 death_dict = dict(zip(pat_df.pat_key, died))
-pat_deaths = [
-    [death_dict[id] for id in np.unique(df.values)]
-    for _, df in trimmed_seq.groupby("medrec_key").pat_key
-]
+pat_deaths = [[death_dict[id] for id in np.unique(df.values)]
+              for _, df in trimmed_seq.groupby("medrec_key").pat_key]
 
 # %% Rolling things up into a dict for easier saving
-pat_dict = {"cv_pats": cv_pats, "pat_lengths": pat_lengths, "pat_deaths": pat_deaths}
+pat_dict = {
+    "cv_pats": cv_pats,
+    "pat_lengths": pat_lengths,
+    "pat_deaths": pat_deaths
+}
 
 # %% Pickling the sequences of visits for loading in the modeling script
 with open(pkl_dir + "int_seqs.pkl", "wb") as f:
--- python/tools/keras.py
+++ python/tools/keras.py
@@ -18,16 +18,12 @@ class DataGenerator(keras.utils.Sequence):
     https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
     
     '''
-    def __init__(self, 
-                 inputs, 
-                 labels,
-                 dim, 
-                 batch_size=32, 
-                 shuffle=True):
+    def __init__(self, inputs, labels, dim, batch_size=32, shuffle=True):
         'Initialization'
 
         # NOTE: Assert that the user is not a clown
-        assert len(inputs) == len(labels), "Inputs and labels are not of equal length"
+        assert len(inputs) == len(
+            labels), "Inputs and labels are not of equal length"
 
         self.dim = dim
         self.batch_size = batch_size
@@ -35,42 +31,43 @@ class DataGenerator(keras.utils.Sequence):
         self.inputs = inputs
         self.shuffle = shuffle
         self.on_epoch_end()
-    
+
     def __len__(self):
         'Denotes the number of batches per epoch'
         return int(np.floor(len(self.inputs) / self.batch_size))
-    
+
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate idx of the batch
-        idx = self.idx[index*self.batch_size:(index+1)*self.batch_size]
-            
+        idx = self.idx[index * self.batch_size:(index + 1) * self.batch_size]
+
         # Generate data
         X, y = self.__data_generation(idx)
-        
+
         return X, y
-    
+
     def on_epoch_end(self):
         'Updates idx after each epoch'
         self.idx = np.arange(len(self.inputs))
         if self.shuffle == True:
             np.random.shuffle(self.idx)
-    
+
     def __data_generation(self, idx):
-        'Generates data containing batch_size samples' 
+        'Generates data containing batch_size samples'
         # Find list of IDs
         bags = [self.inputs[k] for k in idx]
-        
+
         # Padding the feature bags
         padded_bags = [pad_sequences(bag, self.dim[-1]) for bag in bags]
-        
+
         # Padding the visit sequences
         padded_seqs = pad_sequences(padded_bags, self.dim[0], value=[[0]])
-        
+
         # Stacking the visits into a single array
         X = np.stack(padded_seqs).astype(np.uint32)
-        
+
         return X, self.labels[idx]
+
 
 class LSTMHyperModel(HyperModel):
     """LSTM model with hyperparameter tuning.
@@ -105,36 +102,39 @@ class LSTMHyperModel(HyperModel):
             A built/compiled keras model ready for hyperparameter tuning
         """
 
-        inp = Input(
-            shape=(n_timesteps, None if self.ragged else self.n_bags),
-            ragged = self.ragged,
-            batch_size=self.batch_size,
-            name="Input"
-        )
-        emb = Embedding(
-            input_dim=self.n_tokens, output_dim=1,
-            name="Embedding"
-        )(inp)
-        reshape = Reshape((n_timesteps, self.n_tokens), name = "Reshape")(emb)
-        lstm = LSTM(
-            units=hp.Int("LSTM Units", min_value=32, max_value=512, step=32),
-            dropout=hp.Float("LSTM Dropout", min_value=0., max_value=0.9, step=0.05),
-            recurrent_dropout=hp.Float("LSTM Recurrent Dropout", min_value=0., max_value=0.9, step=0.05),
-            name="Recurrent"
-        )(reshape)
-        output = Dense(1, activation="softmax", name = "Output")(lstm)
+        inp = Input(shape=(n_timesteps, None if self.ragged else self.n_bags),
+                    ragged=self.ragged,
+                    batch_size=self.batch_size,
+                    name="Input")
+        emb = Embedding(input_dim=self.n_tokens,
+                        output_dim=1,
+                        name="Embedding")(inp)
+        reshape = Reshape((n_timesteps, self.n_tokens), name="Reshape")(emb)
+        lstm = LSTM(units=hp.Int("LSTM Units",
+                                 min_value=32,
+                                 max_value=512,
+                                 step=32),
+                    dropout=hp.Float("LSTM Dropout",
+                                     min_value=0.,
+                                     max_value=0.9,
+                                     step=0.05),
+                    recurrent_dropout=hp.Float("LSTM Recurrent Dropout",
+                                               min_value=0.,
+                                               max_value=0.9,
+                                               step=0.05),
+                    name="Recurrent")(reshape)
+        output = Dense(1, activation="softmax", name="Output")(lstm)
 
-        model = keras.Model(inp, output, name = "LSTM-Hyper")
+        model = keras.Model(inp, output, name="LSTM-Hyper")
 
         model.compile(
             optimizer=keras.optimizers.Adam(
                 # NOTE: we could also use a LR adjustment callback on train
                 # instead. We could also use any optimizer here
-                learning_rate=hp.Choice("Learning Rate", values=[1e-2, 1e-3, 1e-4])
-            ),
+                learning_rate=hp.Choice("Learning Rate",
+                                        values=[1e-2, 1e-3, 1e-4])),
             # NOTE: Assuming binary classification task, but we could change.
             loss="binary_crossentropy",
-            metrics=["accuracy"]
-        )
+            metrics=["accuracy"])
 
         return model
