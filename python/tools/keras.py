3ed21a2ed348e54c1d2f5c0b109091228d3433d7
Scott Lee <yle4@cdc.gov>
2020-11-18 09:30:09 -0500

    docstrings

--- python/tools/keras.py
+++ python/tools/keras.py
@@ -11,56 +11,79 @@ from keras import backend as K
 
 
 class DataGenerator(keras.utils.Sequence):
-    ''''Generates data for Keras. Code jacked from here:
-    https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+    '''Generates data for Keras.
     
+    Code jacked from here:
+    https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
     '''
     def __init__(self, 
                  inputs, 
-                 labels,
-                 dim, 
+                 dim,
+                 labels=None, 
                  batch_size=32, 
                  shuffle=True):
-        'Initialization'
-        self.dim = dim
+        '''Initializes the data generator. 
+        
+        Args:
+          inputs: either a list of lists of lists of integers, or a list
+            of visit-level tuples of the form ([list of lists of 
+            integers], label)
+          dim: a tuple of the form (max_time, vocab_size) that specifies
+            the goal (i.e., padded) size for a single input example
+          labels: a np.array of sequence-level labels
+          batch_size: size for the minibatches
+          shuffle: whether to shuffle the data after each epoch
+        '''
+        # Setting some basic attributes
+        self.dim = max_time
         self.batch_size = batch_size
-        self.labels = labels
         self.inputs = inputs
         self.shuffle = shuffle
+        
+        # Separating the inputs and labels, in case they were passed as one
+        if labels is None:
+            labels = np.array([tup[1] for tup in inputs])
+            inputs = [tup[0] for tup in inputs]
+        
+        # Shuffling the data to start
         self.on_epoch_end()
     
     def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.inputs) / self.batch_size))
+        '''Returns the number of batches per epoch'''
+        n_batch = int(np.floor(len(self.inputs) / self.batch_size))
+        return n_batch
     
     def __getitem__(self, index):
-        'Generate one batch of data'
+        '''Fetches a batch of X, y pairs given a batch number'''
         # Generate idx of the batch
         idx = self.idx[index*self.batch_size:(index+1)*self.batch_size]
             
-        # Generate data
+        # Generate data for the batch
         X, y = self.__data_generation(idx)
         
         return X, y
     
     def on_epoch_end(self):
-        'Updates idx after each epoch'
+        '''Decides what to do at the end of each epoch, e.g., shuffling'''
+        # Shuffles the data after each epoch
         self.idx = np.arange(len(self.inputs))
         if self.shuffle == True:
             np.random.shuffle(self.idx)
     
     def __data_generation(self, idx):
-        'Generates data containing batch_size samples' 
-        # Find list of IDs
+        '''Yields a batch of X, y pairs given batch indicies'''
+        # Making a list of the visit sequences in the batch
         bags = [self.inputs[k] for k in idx]
         
-        # Padding the feature bags
+        # Padding the feature bags in each visit to V
         padded_bags = [pad_sequences(bag, self.dim[-1]) for bag in bags]
         
-        # Padding the visit sequences
+        # Padding each visit sequence to MAX_TIME
         padded_seqs = pad_sequences(padded_bags, self.dim[0], value=[[0]])
         
-        # Stacking the visits into a single array
+        # Stacking the fully-padded sequences of bags into a single array and
+        # selecting the corresponding labels
         X = np.stack(padded_seqs).astype(np.uint32)
+        y = self.labels[idx]
         
-        return X, self.labels[idx]
+        return X, y
