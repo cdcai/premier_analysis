227abd7e993e0e74c5d9e5a5d3b58d2cdb9e28c6
Scott Lee <yle4@cdc.gov>
2020-11-18 09:30:09 -0500

    docstrings

--- python/tools/keras.py
+++ python/tools/keras.py
@@ -13,128 +13,79 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 class DataGenerator(keras.utils.Sequence):
-    ''''Generates data for Keras. Code jacked from here:
-    https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+    '''Generates data for Keras.
     
+    Code jacked from here:
+    https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
     '''
-    def __init__(self, inputs, labels, dim, batch_size=32, shuffle=True):
-        'Initialization'
-
-        # NOTE: Assert that the user is not a clown
-        assert len(inputs) == len(
-            labels), "Inputs and labels are not of equal length"
-
-        self.dim = dim
+    def __init__(self, 
+                 inputs, 
+                 dim,
+                 labels=None, 
+                 batch_size=32, 
+                 shuffle=True):
+        '''Initializes the data generator. 
+        
+        Args:
+          inputs: either a list of lists of lists of integers, or a list
+            of visit-level tuples of the form ([list of lists of 
+            integers], label)
+          dim: a tuple of the form (max_time, vocab_size) that specifies
+            the goal (i.e., padded) size for a single input example
+          labels: a np.array of sequence-level labels
+          batch_size: size for the minibatches
+          shuffle: whether to shuffle the data after each epoch
+        '''
+        # Setting some basic attributes
+        self.dim = max_time
         self.batch_size = batch_size
-        self.labels = labels
         self.inputs = inputs
         self.shuffle = shuffle
+        
+        # Separating the inputs and labels, in case they were passed as one
+        if labels is None:
+            labels = np.array([tup[1] for tup in inputs])
+            inputs = [tup[0] for tup in inputs]
+        
+        # Shuffling the data to start
         self.on_epoch_end()
 
     def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.inputs) / self.batch_size))
-
+        '''Returns the number of batches per epoch'''
+        n_batch = int(np.floor(len(self.inputs) / self.batch_size))
+        return n_batch
+    
     def __getitem__(self, index):
-        'Generate one batch of data'
+        '''Fetches a batch of X, y pairs given a batch number'''
         # Generate idx of the batch
-        idx = self.idx[index * self.batch_size:(index + 1) * self.batch_size]
-
-        # Generate data
+        idx = self.idx[index*self.batch_size:(index+1)*self.batch_size]
+            
+        # Generate data for the batch
         X, y = self.__data_generation(idx)
 
         return X, y
 
     def on_epoch_end(self):
-        'Updates idx after each epoch'
+        '''Decides what to do at the end of each epoch, e.g., shuffling'''
+        # Shuffles the data after each epoch
         self.idx = np.arange(len(self.inputs))
         if self.shuffle == True:
             np.random.shuffle(self.idx)
 
     def __data_generation(self, idx):
-        'Generates data containing batch_size samples'
-        # Find list of IDs
+        '''Yields a batch of X, y pairs given batch indicies'''
+        # Making a list of the visit sequences in the batch
         bags = [self.inputs[k] for k in idx]
-
-        # Padding the feature bags
+        
+        # Padding the feature bags in each visit to V
         padded_bags = [pad_sequences(bag, self.dim[-1]) for bag in bags]
-
-        # Padding the visit sequences
+        
+        # Padding each visit sequence to MAX_TIME
         padded_seqs = pad_sequences(padded_bags, self.dim[0], value=[[0]])
-
-        # Stacking the visits into a single array
+        
+        # Stacking the fully-padded sequences of bags into a single array and
+        # selecting the corresponding labels
         X = np.stack(padded_seqs).astype(np.uint32)
-
-        return X, self.labels[idx]
-
-
-class LSTMHyperModel(HyperModel):
-    """LSTM model with hyperparameter tuning.
-
-    This is the first-draft LSTM model with a single embedding layer
-    and LSTM layer.
-
-    Args:
-        ragged (bool): Should the input be treated as ragged or dense?
-        n_timesteps (int): length of time sequence
-        n_tokens (int): Vocabulary size for embedding layer
-        n_bags (int): length of maximum bags of text sequences
-        batch_size (int): Training batch size
-    """
-    def __init__(self, ragged, n_timesteps, n_tokens, n_bags, batch_size):
-        # Capture model parameters at init
-        self.ragged = ragged
-        self.n_timesteps = n_timesteps
-        self.n_tokens = n_tokens
-        self.n_bags = n_bags
-        self.batch_size = batch_size
-
-    def build(self, hp):
-        """Build LSTM model 
-
-        Notes:
-            This is normally called within a HyperModel context.
-        Args:
-            hp (:obj:`HyperParameters`): `HyperParameters` instance
-
-        Returns:
-            A built/compiled keras model ready for hyperparameter tuning
-        """
-
-        inp = Input(shape=(self.n_timesteps,
-                           None if self.ragged else self.n_bags),
-                    ragged=self.ragged,
-                    batch_size=self.batch_size,
-                    name="Input")
-        emb = Embedding(input_dim=self.n_tokens,
-                        output_dim=1,
-                        name="Embedding")(inp)
-        reshape = Reshape((self.n_timesteps, self.n_bags), name="Reshape")(emb)
-        lstm = LSTM(units=hp.Int("LSTM Units",
-                                 min_value=32,
-                                 max_value=512,
-                                 step=32),
-                    dropout=hp.Float("LSTM Dropout",
-                                     min_value=0.,
-                                     max_value=0.9,
-                                     step=0.05),
-                    recurrent_dropout=hp.Float("LSTM Recurrent Dropout",
-                                               min_value=0.,
-                                               max_value=0.9,
-                                               step=0.05),
-                    name="Recurrent")(reshape)
-        output = Dense(1, activation="softmax", name="Output")(lstm)
-
-        model = keras.Model(inp, output, name="LSTM-Hyper")
-
-        model.compile(
-            optimizer=keras.optimizers.Adam(
-                # NOTE: we could also use a LR adjustment callback on train
-                # instead. We could also use any optimizer here
-                learning_rate=hp.Choice("Learning Rate",
-                                        values=[1e-2, 1e-3, 1e-4])),
-            # NOTE: Assuming binary classification task, but we could change.
-            loss="binary_crossentropy",
-            metrics=["accuracy"])
-
-        return model
+        y = self.labels[idx]
+        
+        return X, y
