cbf56f456c6deadccaa6771af3af7d0529fd9e9e
beansrowning <sbrowning@cdc.gov>
2020-11-18 20:10:21 -0500

    WIP model work - Updating to new model code, untested

--- python/tools/keras.py
+++ python/tools/keras.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from kerastuner import HyperModel
 from tensorflow import keras as keras
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import LSTM, Dense, Embedding, Input, Reshape
+from tensorflow.keras.layers import LSTM, Dense, Embedding, Input, Reshape, Multiply
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
@@ -96,3 +96,81 @@ class DataGenerator(keras.utils.Sequence):
         y = self.labels[idx]
         
         return X, y
+
+
+class LSTMHyperModel(HyperModel):
+    """LSTM model with hyperparameter tuning.
+
+    This is the first-draft LSTM model with a single embedding layer
+    and LSTM layer.
+
+    Args:
+        ragged (bool): Should the input be treated as ragged or dense?
+        n_timesteps (int): length of time sequence
+        n_tokens (int): Vocabulary size for embedding layer
+        n_bags (int): length of maximum bags of text sequences
+        batch_size (int): Training batch size
+    """
+    def __init__(self, ragged, n_timesteps, n_tokens, n_bags, batch_size):
+        # Capture model parameters at init
+        self.ragged = ragged
+        self.n_timesteps = n_timesteps
+        self.n_tokens = n_tokens
+        self.n_bags = n_bags
+        self.batch_size = batch_size
+
+    def build(self, hp):
+        """Build LSTM model 
+
+        Notes:
+            This is normally called within a HyperModel context.
+        Args:
+            hp (:obj:`HyperParameters`): `HyperParameters` instance
+
+        Returns:
+            A built/compiled keras model ready for hyperparameter tuning
+        """
+
+        inp = Input(shape=(self.n_timesteps, None),
+                    ragged=self.ragged,
+                    batch_size=self.batch_size,
+                    name="Input")
+        emb1 = Embedding(input_dim=self.n_tokens,
+                         output_dim=hp.Int("Embedding Dimension",
+                                           min_value=64,
+                                           max_value=1024,
+                                           step=64),
+                         name="Feature Embeddings")(inp)
+        emb2 = Embedding(input_dim=self.n_tokens,
+                         output_dim=1,
+                         name="Average Embeddings")(inp)
+        mult = Multiply(name="Embeddings x Ave Weights")[emb1, emb2]
+        avg = K.mean(mult, axis=2)
+        lstm = LSTM(units=hp.Int("LSTM Units",
+                                 min_value=32,
+                                 max_value=512,
+                                 step=32),
+                    dropout=hp.Float("LSTM Dropout",
+                                     min_value=0.,
+                                     max_value=0.9,
+                                     step=0.05),
+                    recurrent_dropout=hp.Float("LSTM Recurrent Dropout",
+                                               min_value=0.,
+                                               max_value=0.9,
+                                               step=0.05),
+                    name="Recurrent")(avg)
+        output = Dense(1, activation="sigmoid", name="Output")(lstm)
+
+        model = keras.Model(inp, output, name="LSTM-Hyper")
+
+        model.compile(
+            optimizer=keras.optimizers.Adam(
+                # NOTE: we could also use a LR adjustment callback on train
+                # instead. We could also use any optimizer here
+                learning_rate=hp.Choice("Learning Rate",
+                                        values=[1e-2, 1e-3, 1e-4])),
+            # NOTE: Assuming binary classification task, but we could change.
+            loss="binary_crossentropy",
+            metrics=["accuracy"])
+
+        return model
