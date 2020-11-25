507306b45c6b7c7e2b28801e1d11b5655e170d24
beansrowning <sbrowning@cdc.gov>
2020-11-25 16:20:19 -0500

    Actually setting up the running model

--- python/keras_models.py
+++ python/keras_models.py
@@ -28,7 +28,7 @@ output_dir = os.path.abspath("../output/") + "/"
 data_dir = os.path.abspath("../data/data/") + "/"
 pkl_dir = output_dir + "pkl/"
 
-with open(pkl_dir + "int_seqs.pkl", "rb") as f:
+with open(pkl_dir + "trimmed_seqs.pkl", "rb") as f:
     X_ = pkl.load(f)
 
 with open(pkl_dir + "pat_data.pkl", "rb") as f:
@@ -39,14 +39,19 @@ with open(pkl_dir + "all_ftrs_dict.pkl", "rb") as f:
 
 # %% Determining number of vocab entries
 N_VOCAB = len(vocab)
-# %% HACK: Sampling only a few to prototype:
-X = random.choices(X_, k=SAMPLE_N)
+# %% Create labels
+# MIS-A (Def 1)
+y = [max(x) for x in y_["misa_pt"]]
 
-# HACK: Randomly generating labels to prototype, remove before moving on
-y = np.random.randint(low=0, high=2, size=len(X))
+# MIS-A (Def 2)
+y2 = [max(x) for x in y_["misa_resp"]]
+
+# Death
+y3 = [max(x) for x in y_["death"]]
 
 # %% Create data generator for On-the-fly batch generation
-dat_generator = tk.DataGenerator(inputs = X, labels = y,
+dat_generator = tk.DataGenerator(inputs=X,
+                                 labels=y,
                                  max_time=TIME_SEQ,
                                  batch_size=BATCH_SIZE)
 
@@ -112,5 +117,3 @@ else:
     # %% Train
     # NOTE: Multiprocessing is superfluous here with epochs=1, but we could use it
     model.fit(dat_generator)
-
-# %%
