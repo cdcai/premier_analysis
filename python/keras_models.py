08460ed7c2c072b2f7ab093a2d12f06e9a049e88
beansrowning <sbrowning@cdc.gov>
2020-11-26 11:49:25 -0500

    TTV split, and TB callback

--- python/keras_models.py
+++ python/keras_models.py
@@ -4,25 +4,31 @@ Starter Keras model
 # %%
 import itertools
 import os
+import csv
 import pickle as pkl
 import random
+from datetime import datetime
 
 import kerastuner.tuners as tuners
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.keras.callbacks import TensorBoard
+from sklearn.model_selection import train_test_split
 
 from tools import keras as tk
 
 # %% Globals
-SAMPLE_N = 10000
 TIME_SEQ = 225
 LSTM_DROPOUT = 0.1
 LSTM_RECURRENT_DROPOUT = 0.2
 N_LSTM = 128
 HYPER_TUNING = False
 BATCH_SIZE = 32
-
+TEST_SPLIT = 0.2
+VAL_SPLIT = 0.25
+RAND = 2020
+TB_UPDATE_FREQ = 1000
 # %% Load in Data
 output_dir = os.path.abspath("output/") + "/"
 data_dir = os.path.abspath("data/data/") + "/"
@@ -37,14 +43,31 @@ with open(pkl_dir + "pat_data.pkl", "rb") as f:
 with open(pkl_dir + "all_ftrs_dict.pkl", "rb") as f:
     vocab = pkl.load(f)
 
+# %% Save Embedding metadata
+with open(output_dir + 'emb_metadata.tsv', 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerows(zip(['id'], ['word']))
+    writer.writerows(zip([0], ['OOV']))
+    for key, value in vocab.items():
+        writer.writerow([value, key])
 # %% Determining number of vocab entries
 N_VOCAB = len(vocab) + 1
 
-# %% Create data generator for On-the-fly batch generation
-dat_generator = tk.DataGenerator(inputs,
-                                 max_time=TIME_SEQ,
-                                 batch_size=BATCH_SIZE)
+# %% Split into test/train
+train, test = train_test_split(inputs, test_size=TEST_SPLIT, random_state=RAND)
 
+# Further split into train/validation
+train, validation = train_test_split(train,
+                                     test_size=VAL_SPLIT,
+                                     random_state=RAND)
+# %% Create data generator for On-the-fly batch generation
+train_gen = tk.DataGenerator(train, max_time=TIME_SEQ, batch_size=BATCH_SIZE)
+
+validation_gen = tk.DataGenerator(validation,
+                                  max_time=TIME_SEQ,
+                                  batch_size=BATCH_SIZE)
+
+test_gen = tk.DataGenerator(test, max_time=TIME_SEQ, batch_size=BATCH_SIZE)
 # %% Model
 
 if HYPER_TUNING:
@@ -106,6 +129,21 @@ else:
 
     model.summary()
 
+    # Create Tensorboard callback
+    tb_callback = TensorBoard(log_dir=output_dir + "tensorboard" +
+                              datetime.now().strftime("%Y%m%d-%H%M%S") + "/",
+                              histogram_freq=TB_UPDATE_FREQ,
+                              update_freq=TB_UPDATE_FREQ,
+                              embeddings_freq=TB_UPDATE_FREQ,
+                              embeddings_metadata=output_dir +
+                              'emb_metadata.tsv')
     # %% Train
     # NOTE: Multiprocessing is superfluous here with epochs=1, but we could use it
-    model.fit(dat_generator)
+    fitting = model.fit(train_gen,
+                        validation_data=validation_gen,
+                        callbacks=[tb_callback])
+
+    test_loss, test_acc = model.evaluate(test_gen)
+
+    print('Test Loss: {}'.format(test_loss))
+    print('Test Accuracy: {}'.format(test_acc))
