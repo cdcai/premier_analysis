efc506db71d83edf3a987de8a1cf8cc072e6afc3
Sean Browning <Sean.Browning94@gmail.com>
2020-12-01 23:57:48 -0500

    Re-running model - Using argmax incorrectly doesn't help.

--- python/keras_models.py
+++ python/keras_models.py
@@ -13,6 +13,7 @@ import kerastuner.tuners as tuners
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
+from imblearn.over_sampling import RandomOverSampler
 from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.utils import compute_class_weight
@@ -22,21 +23,25 @@ from tools import keras as tk
 
 # %% Globals
 TIME_SEQ = 225
-LSTM_DROPOUT = 0.1
+LSTM_DROPOUT = 0.2
+# NOTE: Recurrent dropout is advisable, but it also means
+# you forgoe CuDNN-optimization for the LSTM, so it will train
+# about 1/3 slower on GPU
 LSTM_RECURRENT_DROPOUT = 0.4
 N_LSTM = 128
 HYPER_TUNING = False
-BATCH_SIZE = 32
+# NOTE: I maxed out my GPU running 32, 20 ran ~7.8GB on GPU
+BATCH_SIZE = 20
 SUBSAMPLE = True
-SAMPLE_FRAC = 0.001
+SAMPLE_FRAC = 0.2
 TEST_SPLIT = 0.2
 VAL_SPLIT = 0.1
 RAND = 2020
 TB_UPDATE_FREQ = 100
 # %% Load in Data
-output_dir = os.path.abspath("../output/") + "/"
-tensorboard_dir = os.path.abspath("../data/model_checkpoints/") + "/"
-data_dir = os.path.abspath("../data/data/") + "/"
+output_dir = os.path.abspath("output/") + "/"
+tensorboard_dir = os.path.abspath("data/model_checkpoints/") + "/"
+data_dir = os.path.abspath("data/data/") + "/"
 pkl_dir = output_dir + "pkl/"
 
 with open(pkl_dir + "trimmed_seqs.pkl", "rb") as f:
@@ -53,10 +58,10 @@ with open(pkl_dir + "feature_lookup.pkl", "rb") as f:
 
 # %% Save Embedding metadata
 # We can use this with tensorboard to visualize the embeddings
-with open(output_dir + 'emb_metadata.tsv', 'w') as f:
-    writer = csv.writer(f, delimiter='\t')
-    writer.writerows(zip(['id'], ['word'], ["desc"]))
-    writer.writerows(zip([0], ['OOV'], ["Padding/OOV"]))
+with open(output_dir + "emb_metadata.tsv", "w") as f:
+    writer = csv.writer(f, delimiter="\t")
+    writer.writerows(zip(["id"], ["word"], ["desc"]))
+    writer.writerows(zip([0], ["OOV"], ["Padding/OOV"]))
     for key, value in vocab.items():
         writer.writerow([value, key, all_feats[key]])
 # %% Determining number of vocab entries
@@ -64,45 +69,57 @@ N_VOCAB = len(vocab) + 1
 
 # %% Subsampling if desired
 if SUBSAMPLE:
-    _, inputs, _, _ = train_test_split(inputs, [labs for _, labs in inputs],
-                                       test_size=SAMPLE_FRAC,
-                                       random_state=RAND,
-                                       stratify=[labs for _, labs in inputs])
+    _, inputs, _, _ = train_test_split(
+        inputs,
+        [labs for _, labs in inputs],
+        test_size=SAMPLE_FRAC,
+        random_state=RAND,
+        stratify=[labs for _, labs in inputs],
+    )
 # %% Split into test/train
-train, test, _, _ = train_test_split(inputs, [labs for _, labs in inputs],
-                                     test_size=TEST_SPLIT,
-                                     random_state=RAND,
-                                     stratify=[labs for _, labs in inputs])
+train, test, _, _ = train_test_split(
+    inputs,
+    [labs for _, labs in inputs],
+    test_size=TEST_SPLIT,
+    random_state=RAND,
+    stratify=[labs for _, labs in inputs],
+)
 
 # Further split into train/validation
 train, validation, _, _ = train_test_split(
-    train, [labs for _, labs in train],
+    train,
+    [labs for _, labs in train],
     test_size=VAL_SPLIT,
     random_state=RAND,
-    stratify=[labs for _, labs in train])
+    stratify=[labs for _, labs in train],
+)
 
 # %% Compute class weights
-class_weights = compute_class_weight('balanced',
-                                     np.unique([labs for _, labs in train]),
-                                     [labs for _, labs in train])
+class_weights = compute_class_weight(
+    class_weight="balanced",
+    classes=np.unique([labs for _, labs in train]),
+    y=[labs for _, labs in train],
+)
 
 class_weights = dict(zip([0, 1], class_weights))
 # %% Create data generator for On-the-fly batch generation
-train_gen = tk.DataGenerator(train, max_time=TIME_SEQ, batch_size=BATCH_SIZE)
-
-validation_gen = tk.DataGenerator(validation,
-                                  max_time=TIME_SEQ,
-                                  batch_size=BATCH_SIZE)
+train_gen = tk.DataGenerator(
+    train,
+    max_time=TIME_SEQ,
+    # resampler=RandomOverSampler(sampling_strategy="minority"),
+    batch_size=BATCH_SIZE,
+)
+# %%
+validation_gen = tk.DataGenerator(validation, max_time=TIME_SEQ, batch_size=BATCH_SIZE)
 
 test_gen = tk.DataGenerator(test, max_time=TIME_SEQ, batch_size=BATCH_SIZE)
 # %% Model
 
 if HYPER_TUNING:
     # Generate Hyperparameter model
-    hyper_model = tk.LSTMHyperModel(ragged=False,
-                                    n_timesteps=TIME_SEQ,
-                                    vocab_size=N_VOCAB,
-                                    batch_size=BATCH_SIZE)
+    hyper_model = tk.LSTMHyperModel(
+        ragged=False, n_timesteps=TIME_SEQ, vocab_size=N_VOCAB, batch_size=BATCH_SIZE
+    )
     tuner = tuners.Hyperband(
         hyper_model,
         objective="accuracy",
@@ -110,7 +127,8 @@ if HYPER_TUNING:
         project_name="hyperparameter-tuning",
         # NOTE: This could be in output as well if we don't want to track/version it
         directory="data/model_checkpoints/",
-        distribution_strategy=tf.distribute.MirroredStrategy())
+        distribution_strategy=tf.distribute.MirroredStrategy(),
+    )
 
     # Announce the search space
     tuner.search_space_summary()
@@ -132,27 +150,26 @@ else:
         batch_size=BATCH_SIZE,
     )
     # Feature Embeddings
-    emb1 = keras.layers.Embedding(N_VOCAB,
-                                  output_dim=512,
-                                  mask_zero=True,
-                                  name="Feature_Embeddings")(input_layer)
+    emb1 = keras.layers.Embedding(
+        N_VOCAB, output_dim=512, mask_zero=True, name="Feature_Embeddings"
+    )(input_layer)
     # Average weights of embedding
-    emb2 = keras.layers.Embedding(N_VOCAB,
-                                  output_dim=1,
-                                  mask_zero=True,
-                                  name="Average_Embeddings")(input_layer)
+    emb2 = keras.layers.Embedding(
+        N_VOCAB, output_dim=1, mask_zero=True, name="Average_Embeddings"
+    )(input_layer)
 
     # Multiply and average
     mult = keras.layers.Multiply(name="Embeddings_by_Average")([emb1, emb2])
     avg = keras.backend.mean(mult, axis=2)
 
-    lstm_layer = keras.layers.LSTM(N_LSTM,
-                                   dropout=LSTM_DROPOUT,
-                                   recurrent_dropout=LSTM_RECURRENT_DROPOUT,
-                                   name="Recurrent")(avg)
+    lstm_layer = keras.layers.LSTM(
+        N_LSTM,
+        dropout=LSTM_DROPOUT,
+        recurrent_dropout=LSTM_RECURRENT_DROPOUT,
+        name="Recurrent",
+    )(avg)
 
-    output_dim = keras.layers.Dense(1, activation="sigmoid",
-                                    name="Output")(lstm_layer)
+    output_dim = keras.layers.Dense(1, activation="sigmoid", name="Output")(lstm_layer)
 
     model = keras.Model(input_layer, output_dim)
 
@@ -161,60 +178,60 @@ else:
     model.summary()
 
     # Create Tensorboard callback
-    tb_callback = TensorBoard(log_dir=tensorboard_dir +
-                              datetime.now().strftime("%Y%m%d-%H%M%S") + "/",
-                              histogram_freq=1,
-                              update_freq=TB_UPDATE_FREQ,
-                              embeddings_freq=1,
-                              embeddings_metadata=output_dir +
-                              'emb_metadata.tsv')
+    tb_callback = TensorBoard(
+        log_dir=tensorboard_dir + datetime.now().strftime("%Y%m%d-%H%M%S") + "/",
+        histogram_freq=1,
+        update_freq=TB_UPDATE_FREQ,
+        embeddings_freq=1,
+        embeddings_metadata=output_dir + "emb_metadata.tsv",
+    )
 
     # Create model checkpoint callback
     model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        filepath=tensorboard_dir + datetime.now().strftime("%Y%m%d-%H%M%S") +
-        "-weights.{epoch:02d}-{val_loss:.2f}.hdf5",
+        filepath=tensorboard_dir
+        + datetime.now().strftime("%Y%m%d-%H%M%S")
+        + "-weights.{epoch:02d}-{val_loss:.2f}.hdf5",
         save_weights_only=True,
-        monitor='val_acc',
-        mode='max',
-        save_best_only=True)
+        monitor="val_acc",
+        mode="max",
+        save_best_only=True,
+    )
 
     # Create early stopping callback
     stopping_checkpoint = keras.callbacks.EarlyStopping(
-        monitor='val_loss',
+        monitor="val_loss",
         min_delta=0,
         patience=2,
-        mode='auto',
-        restore_best_weights=True)
+        mode="auto",
+        restore_best_weights=True,
+    )
 
     # %% Train
-    fitting = model.fit(train_gen,
-                        validation_data=validation_gen,
-                        epochs=20,
-                        class_weight=class_weights,
-                        callbacks=[
-                            tb_callback, model_checkpoint_callback,
-                            stopping_checkpoint
-                        ])
+    fitting = model.fit(
+        train_gen,
+        validation_data=validation_gen,
+        epochs=20,
+        class_weight=class_weights,
+        callbacks=[tb_callback, model_checkpoint_callback, stopping_checkpoint],
+    )
 
     # Test
     test_loss, test_acc = model.evaluate(test_gen)
 
-    print('Test Loss: {}'.format(test_loss))
-    print('Test Accuracy: {}'.format(test_acc))
+    print("Test Loss: {}".format(test_loss))
+    print("Test Accuracy: {}".format(test_acc))
 
     # %% F1, etc
     pred = model.predict(test_gen)
-    y_pred = np.argmax(pred, axis=1)
+    y_pred = (pred >= 0.5).astype(int)
     y_true = [lab for _, lab in test]
 
     # Resizing for output which is divisible by BATCH_SIZE
-    y_true = y_true[0:y_pred.shape[0]]
+    y_true = y_true[0 : y_pred.shape[0]]
 
-    output = classification_report(y_true,
-                                   y_pred,
-                                   target_names=["Non MIS-A", "MIS-A"])
+    output = classification_report(y_true, y_pred, target_names=["Non MIS-A", "MIS-A"])
 
     print(output)
-    print('ROC-AUC: {}'.format(roc_auc_score(y_true, y_pred)))
+    print("ROC-AUC: {}".format(roc_auc_score(y_true, y_pred)))
 
 # %%
