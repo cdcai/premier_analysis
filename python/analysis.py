2b02887007fc21dc8c7ba997d4064385d2ed1975
Sean Browning <sbrowning@cdc.gov>
2021-03-16 16:01:07 -0400

    parameterizing bootCIs

--- python/analysis.py
+++ python/analysis.py
@@ -1,37 +1,59 @@
+import argparse
+import os
+
 import numpy as np
 import pandas as pd
-import os
 
 import tools.analysis as ta
 import tools.multi as tm
 
+if __name__ == "__main__":
 
-# Setting the directories
-output_dir = os.path.abspath('output/') + '/'
-stats_dir = output_dir + 'analysis/'
+    parser = argparse.ArgumentParser()
 
-# Importing the predictions
-death_preds = pd.read_csv(stats_dir + 'death_preds.csv')
-multi_preds = pd.read_csv(stats_dir + 'multi_class_preds.csv')
+    parser.add_argument(
+        '--day_one',
+        help="Use only first inpatient day's worth of features (DAN only)",
+        dest='day_one',
+        action='store_true')
+    parser.add_argument('--all_days',
+                        help="Use all features in lookback period (DAN only)",
+                        dest='day_one',
+                        action='store_false')
+    parser.set_defaults(day_one=True)
+    parser.add_argument("--outcome",
+                        type=str,
+                        default="misa_pt",
+                        choices=["misa_pt", "multi_class", "death"],
+                        help="which outcome to use as the prediction target")
 
-# Setting the models to look at
-mods = ['lgr_d1', 'rf_d1', 'gbc_d1', 'svm_d1', 'dan_d1']
+    args = parser.parse_args()
 
-# Running the single confidence intervals (this takes a little time)
-death_cis = [ta.boot_cis(death_preds.death,
-                         death_preds[mod + '_pred'],
-                         n=1000) 
-             for mod in mods]
-multi_cis = [ta.boot_cis(multi_preds.multi_class,
-                         multi_preds[mod + '_pred'],
-                         n=1000) 
-             for mod in mods]
-cis = [death_cis, multi_cis]
-outcomes = ['death', 'multi']
+    DAY_ONE = args.day_one
+    OUTCOME = args.outcome
 
-# Writing the confidence intervals to disk
-for i, outcome in enumerate(outcomes):
-    writer = pd.ExcelWriter(stats_dir + outcome + '_cis.xlsx')
-    for i, obj in enumerate(cis[i]):
-        obj.cis.to_excel(writer, sheet_name=mods[i])
-    writer.save()
+    # Setting the directories
+    pwd = os.path.abspath(os.path.dirname(__file__))
+    output_dir = os.path.join(pwd, "..", "output", "")
+    stats_dir = os.path.join(output_dir, "analysis", "")
+
+    # Importing the predictions
+    preds = pd.read_csv(stats_dir + OUTCOME + "_preds.csv")
+
+    # Setting the models to look at
+    mods = ['lgr', 'rf', 'gbc', 'svm', 'dan', 'lstm']
+
+    if DAY_ONE:
+        mods = [mod + "_d1" for mod in mods if mod != "lstm"]
+
+    # Running the single confidence intervals (this takes a little time)
+    cis = [
+        ta.boot_cis(preds[OUTCOME], preds[mod + '_pred'], n=1000)
+        for mod in mods
+    ]
+
+    # Writing the confidence intervals to disk
+    with pd.ExcelWriter(stats_dir + OUTCOME + '_cis.xlsx') as writer:
+        for i, obj in enumerate(cis):
+            obj.cis.to_excel(writer, sheet_name=mods[i])
+        writer.save()
