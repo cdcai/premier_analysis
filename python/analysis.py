ef89a488c00bc6d12adaea7ac5561e49746ba811
Scott Lee <yle4@cdc.gov>
2021-04-08 15:02:55 -0400

    added cutpoint support for boot_cis

--- python/analysis.py
+++ python/analysis.py
@@ -1,3 +1,5 @@
+'''This script generates bootstrap CIs for the models and metrics'''
+
 import numpy as np
 import pandas as pd
 import pickle
@@ -14,11 +16,12 @@ stats_dir = output_dir + 'analysis/'
 # Importing the predictions
 death_preds = pd.read_csv(stats_dir + 'death_preds.csv')
 multi_preds = pd.read_csv(stats_dir + 'multi_class_preds.csv')
+misa_pt_preds = pd.read_csv(stats_dir + 'misa_pt_preds.csv')
 
 # Setting the models to look at
-mods = ['lgr_d1', 'rf_d1', 'gbc_d1', 'svm_d1', 'dan_d1']
-pred_dfs = [death_preds, multi_preds]
-outcomes = ['death', 'multi', 'misa_pt']
+mods = ['lgr_d1', 'rf_d1', 'gbc_d1', 'svm_d1', 'dan_d1', 'lstm']
+pred_dfs = [death_preds, multi_preds, misa_pt_preds]
+outcomes = ['death', 'multi_class', 'misa_pt']
 cis = []
 
 # Running the single confidence intervals (this takes a little time)
@@ -30,14 +33,18 @@ for i, outcome in enumerate(outcomes):
         mod_probs = mod + '_' + outcome + '.pkl'
         if mod_probs in prob_files:
             with open(probs_dir + mod_probs, 'rb') as f:
-                guesses = pickle.load(f)
+                prob_dict = pickle.load(f)
+                cutpoint = prob_dict['cutpoint']
+                guesses = prob_dict['probs']    
         else:
+            cutpoint = 0.5
             guesses = pred_dfs[i][mod + '_pred']
-            
+        
         ci = ta.boot_cis(targets=pred_dfs[i][outcome],
                          guesses=guesses,
+                         cutpoint=cutpoint,
                          n=100)
-        pred_cis.append(ci)
+        outcome_cis.append(ci)
     cis.append(ta.merge_ci_list(outcome_cis, mod_names=mods))
 
 # Writing the confidence intervals to disk
