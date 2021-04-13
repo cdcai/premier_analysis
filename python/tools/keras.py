636a879574eed7e3bdf8adbe2e9b85fe7ac39bd6
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2021-04-13 17:35:39 -0400

    Add FocalLoss - I swear I did this at some point

--- python/tools/keras.py
+++ python/tools/keras.py
@@ -67,6 +67,7 @@ def create_ragged_data_gen(inputs: list,
     elif demog_output == "one-hot":
         demog = sequence_to_onehot_tensor([dem for _, dem, _ in inputs])
     else:
+        # Take as sequence
         # BUG: Model doesn't seem to like this when it's ragged. Figure it out eventually.
         demog = tf.ragged.constant([dem for _, dem, _ in inputs])
         demog = demog.to_tensor(default_value=0,
@@ -541,3 +542,46 @@ def DAN(vocab_size,
                                 name='output')(dense)
 
     return keras.Model(input, output)
+
+# Jacked from https://github.com/Tony607/Focal_Loss_Keras/blob/master/src/keras_focal_loss.ipynb
+class FocalLoss(keras.losses.Loss):
+    def __init__(self, gamma=2., alpha=4.,
+                 reduction=keras.losses.Reduction.AUTO, name='focal_loss'):
+        """Focal loss for multi-classification
+        FL(p_t)=-alpha(1-p_t)^{gamma}ln(p_t)
+        Notice: y_pred is probability after softmax
+        gradient is d(Fl)/d(p_t) not d(Fl)/d(x) as described in paper
+        d(Fl)/d(p_t) * [p_t(1-p_t)] = d(Fl)/d(x)
+        Focal Loss for Dense Object Detection
+        https://arxiv.org/abs/1708.02002
+
+        Keyword Arguments:
+            gamma {float} -- (default: {2.0})
+            alpha {float} -- (default: {4.0})
+        """
+        super(FocalLoss, self).__init__(reduction=reduction,
+                                        name=name)
+        self.gamma = float(gamma)
+        self.alpha = float(alpha)
+
+    def call(self, y_true, y_pred):
+        """
+        Arguments:
+            y_true {tensor} -- ground truth labels, shape of [batch_size, num_cls]
+            y_pred {tensor} -- model's output, shape of [batch_size, num_cls]
+
+        Returns:
+            [tensor] -- loss.
+        """
+        epsilon = 1.e-9
+        y_true = tf.convert_to_tensor(y_true, tf.float32)
+        y_pred = tf.convert_to_tensor(y_pred, tf.float32)
+
+        model_out = tf.add(y_pred, epsilon)
+        ce = tf.multiply(y_true, -tf.math.log(model_out))
+        weight = tf.multiply(y_true, tf.pow(
+            tf.subtract(1., model_out), self.gamma))
+        fl = tf.multiply(self.alpha, tf.multiply(weight, ce))
+        reduced_fl = tf.reduce_max(fl, axis=1)
+        
+        return tf.reduce_mean(reduced_fl)
